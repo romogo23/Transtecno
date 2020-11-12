7d7ba1ad45a0b0d855c52b66af59215d7bb8a020
Alexandra Cordero <alexandra.cordero@ucrso.info>
2020-11-11 23:40:38 -0600

    Agregué las clases de user y Remainder al Dom

---
+++ DOM/Remainder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOM
+{
+    class Remainder
+    {
+        private int idRemainder { get; set; }
+        private int invoiceNumberSupplier { get; set; }
+        private int invoiceNumberClient { get; set; }
+        private string description { get; set; }
+        private string userName { get; set; }
+        private DateTime dateRemainder { get; set; }
+        private string email { get; set; }
+
+        public Remainder(int invoiceNumberSupplier, int invoiceNumberClient, string description, string userName,
+                DateTime dateRemainder, string email)
+        {
+            this.invoiceNumberSupplier = invoiceNumberSupplier;
+            this.invoiceNumberClient = invoiceNumberClient;
+            this.description = description;
+            this.userName = userName;
+            this.dateRemainder = dateRemainder;
+            this.email = email;
+        }
+    }
+}
---
+++ DOM/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOM
+{
+    class User
+    {
+        private string username { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+        private int rol { get; set; }
+
+
+        public User(string username, string email, string password, int rol) 
+        {
+            this.username = username;
+            this.email = email;
+            this.password = password;
+            this.rol = rol;
+        }
+    }
+}
