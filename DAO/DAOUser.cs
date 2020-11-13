a32e7e307fadebd68b0b0f52b5cba7c25b0761a9
Hilary <hilary.ramirez@ucrso.info>
2020-11-12 20:55:23 -0600

    Agregué la clase DAOUser

---
+++ DAO/DAOUser.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    class DAOUser
+    {
+        SqlConnection conn = new SqlConnection("");
+
+        public Boolean insertUser() {
+            return true;
+
+        }
+        public Boolean verifyClient(string id)
+        {
+            Boolean existsC;
+            string query = "SELECT CASE WHEN EXISTS ( SELECT *FROM [Cliente] WHERE (Id_Cliente = @Id_Cliente)) THEN CAST(1 AS BIT) ELSE CAST(0 AS BIT) END ";
+            SqlCommand comm2 = new SqlCommand(query, conn);
+            comm2.Parameters.AddWithValue("@Id_Cliente", id);
+            if (conn.State != System.Data.ConnectionState.Open)
+            {
+                conn.Open();
+            }
+
+            existsC = (Boolean)comm2.ExecuteScalar();
+
+
+            if (conn.State != System.Data.ConnectionState.Closed)
+            {
+                conn.Close();
+
+            }
+            return existsC;
+
+        }
+
+    }
+}
